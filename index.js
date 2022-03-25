e77c0f2e85a75560a78723d72d5a5ccc7102fc6e
Mwagiru <joymwagiru@gmail.com>
2022-03-25 16:04:11 +0300

    Almost done

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b){
+    return a + b;
+}
+add(1, 2);
+function subtract(a, b) {
+    return a - b;
+}
+subtract(17, 7);
+function multiply(a, b) {
+    return a * b;
+}
+multiply(1, 2);
+function divide(a, b) {
+    return a / b;
+}
+divide(6, 2);
+function increment(number){
+    return ++number;
+}
+increment(6);
+function decrement(number) {
+    return --number;
+}
+decrement(6);
+function makeInt(number){
+    return parseInt(number);
+}
+makeInt("0" , 10);
+function preserveDecimal(number){
+    return parseFloat(number);
+}
+preserveDecimal("$67");
